a817803c2b9ce4470431ee06d4ccb83df0c70d26
AgileBelma <agilebelma@gmail.com>
2017-12-11 12:27:25 +1300

    Add css and javascript

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', startQuote);
+
+function startQuote() {
+
+  console.log("startQuote is called");
+  document.getElementById('buttonNewQuote').addEventListener("click", loadQuotes);
+  //document.getElementById('buttonNewQuote').onclick = loadQuotes;
+
+}
+
+function loadQuotes() {
+
+  console.log("loadQuotes is called");
+  var maoriQuote = document.getElementById('maoriQuote');
+  var englishQuote = document.getElementById('englishQuote');
+
+  maoriQuote.innerHTML = "Tuhinga!";
+  englishQuote.innerHTML = "Success!";
+}
+
+
